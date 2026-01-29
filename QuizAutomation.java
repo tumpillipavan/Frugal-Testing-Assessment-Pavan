481bbdd100b01a83b4a23823eef6d277a10164d3
tumpillipavan <117649437+tumpillipavan@users.noreply.github.com>
2026-01-29 19:14:29 +0530

    Update QuizAutomation.java

--- QuizAutomation.java
+++ QuizAutomation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler; // Import for file saving
+import org.openqa.selenium.io.FileHandler; 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.Select;
